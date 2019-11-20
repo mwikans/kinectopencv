b15838ac351e7ea10252bf94e76df3731077e155
mwikans <39635934+mwikans@users.noreply.github.com>
2019-11-20 15:53:55 +0700

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -1,20 +1,36 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+#class KinectRuntime(object):
+    #def __init__(self):
+    #    self._done = False
 
-while(True):
+    #def show_camera(self):
+cap = cv2.VideoCapture(0)
+while True:
+
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    green_image = frame.copy()
+    green_image[:,:,0] = 0
+    green_image[:,:,2] = 0
+
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = frame#cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    _image = cv2.applyColorMap(green_image, cv2.COLORMAP_JET)
 
     # Display the resulting frame
     cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv2.imshow('gframe', green_image)
+    cv2.imshow('jframe', _image)
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    break
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+#__main__ = "Kinect Camera"
+#run_ = KinectRuntime();
+#run_.show_camera();
